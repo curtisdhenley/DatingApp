8dfcc233aeab78ec7d4a9d096c2054dd7355ffc9
Curtis Henley <curtisdhenley@gmail.com>
2023-04-12 14:13:33 -0400

    disabled hot reload for dotnet. added user pw hash

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,6 +4,8 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         // public string FirstName { get; set; }
         // public string LastName { get; set; }
         // public string FullName { get { return $"{FirstName} {LastName}"; } }
